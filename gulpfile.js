8a75b1d46b3fd61ab26465a92ce9ac2455620f91
Sergey Khmelevskoy <xs290@me.com>
2018-02-07 19:30:04 +0300

    fail

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,12 @@ var notify = require("gulp-notify");
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var svgmin = require('gulp-svgmin');
-var fontgen = require('gulp-fontgen');
-var tinypng = require('gulp-tinypng-compress');
+// var fontgen = require('gulp-fontgen');
+// var tinypng = require('gulp-tinypng-compress');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var compass = require('gulp-compass');
 
 //Setting Project
 var pathProject = 'stopsrach';
@@ -21,12 +22,7 @@ var browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
 gulp.task('styles', function () {
-    return gulp.src('web/sass/*.scss')
-        .pipe(sass({
-            outputStyle: config.production ? 'compact' : 'expanded', // nested, expanded, compact, compressed
-            precision: 5,
-            includePaths : ['web/sass']
-        }))
+    return gulp.src('web/css/style.css')
         .pipe(plumber())
        // .pipe(sourcemaps.init())
         .pipe(autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
@@ -35,6 +31,7 @@ gulp.task('styles', function () {
         .pipe(gulp.dest('web/dist/css/'))
         .pipe(notify("Done CSS!"));
 });
+
 /***********************************************
  Default Gulp
  ***********************************************/
@@ -48,9 +45,9 @@ gulp.task('serve', ['styles'], function() {
     //     proxy: "http://vitime.akvion/"
     // });
     browserSync.init({
-      proxy: 'https://dev.vitime.pro/produkty/antioxidants/',
+      proxy: 'http://dev.vitime.pro/produkty/antioxidants/',
       files: ['web/dist/css/' + '*.css'],
-      middleware: require('serve-static')('./web/dist'),
+      middleware: require('serve-static')('web/dist'),
       rewriteRules: [
         {
           match: new RegExp('</head>'),
@@ -62,13 +59,13 @@ gulp.task('serve', ['styles'], function() {
       port: 8080
     })
 
-    gulp.watch("web/sass/*.scss",['styles']);
+    gulp.watch("web/css/*.css",['styles']);
     gulp.watch("web/img/svg/*.svg",['svg']);
-    gulp.watch("web/js/app.js",['script:app']);
-    gulp.watch("web/js/index.js",['script:index']);
-    gulp.watch("web/js/*.js").on('change', reload);
+    // gulp.watch("web/js/app.js",['script:app']);
+    // gulp.watch("web/js/index.js",['script:index']);
+    // gulp.watch("web/js/*.js").on('change', reload);
     gulp.watch("web/dist/css/*.css").on('change', reload);
-    gulp.watch("views/**/*.php").on('change', reload);
+    // gulp.watch("views/**/*.php").on('change', reload);
 });
 /***********************************************
  JS Compress
@@ -140,15 +137,15 @@ gulp.task('svg', function () {
 /***********************************************
  Generate Fonts
  ***********************************************/
-gulp.task('fonts:web', function() {
-    return gulp.src('web/dist/css/fonts/MyriadPro-SemiBold/*.{ttf,otf}')
-        .on('data',function(file){
-            console.log({
-                base:file.relative
-            });
-        })
-        .pipe(fontgen({
-            dest: "web/dist/css/fonts/MyriadPro-SemiBold/"
-        }))
-        .pipe(notify("Done Font!"));
-});
+// gulp.task('fonts:web', function() {
+//     return gulp.src('web/dist/css/fonts/MyriadPro-SemiBold/*.{ttf,otf}')
+//         .on('data',function(file){
+//             console.log({
+//                 base:file.relative
+//             });
+//         })
+//         .pipe(fontgen({
+//             dest: "web/dist/css/fonts/MyriadPro-SemiBold/"
+//         }))
+//         .pipe(notify("Done Font!"));
+// });
